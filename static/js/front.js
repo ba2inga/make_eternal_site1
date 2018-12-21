e57893c610dd19f14e104b39eedbc04b13154a1d
ba2inga <jikimicool@naver.com>
2018-12-21 17:18:09 +0900

    gg
    gg

--- static/js/front.js
+++ static/js/front.js
@@ -123,7 +123,7 @@ function sliders () {
     $('.homepage').owlCarousel({
       navigation: false, // Show next and prev buttons
       navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-      slideSpeed: 2000,
+      slideSpeed: 5000,
       paginationSpeed: 1000,
       autoPlay: true,
       stopOnHover: true,
