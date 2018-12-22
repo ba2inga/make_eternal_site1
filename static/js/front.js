c49a19329f0e848e99709ee414876e4c2142dfaa
make_eternal <jikimicool@naver.com>
2018-12-22 17:45:22 +0900

    eagfg
    dj

--- static/js/front.js
+++ static/js/front.js
@@ -123,7 +123,7 @@ function sliders () {
     $('.homepage').owlCarousel({
       navigation: false, // Show next and prev buttons
       navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-      slideSpeed: 15000,
+      slideSpeed: 1500,
       paginationSpeed: 1000,
       autoPlay: true,
       stopOnHover: true,
